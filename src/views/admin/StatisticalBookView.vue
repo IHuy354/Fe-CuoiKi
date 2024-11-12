cd78cac1f3368d7061ecfba8a95c8e2000746576
IHuy354 <leduchuy.vng2004@gmail.com>
2024-11-12 09:17:48 +0700

    thongkeds_fistday

--- src/views/admin/StatisticalBookView.vue
+++ src/views/admin/StatisticalBookView.vue
@@ -1,9 +1,52 @@
 <template>
-    <h1>thống kê danh sách yêu thích </h1>
+  <div class="container">
+    <h2>BẢNG THỐNG KÊ</h2>
+    <div class="thongke">
+      <!-- trái -->
+      <!-- 1 -->
+      <div class="thong-ke__list bg__section">
+        <div class="thong-ke__soluong">
+          <h2 class="thong-ke__dem">25,119</h2>
+          <img src="" alt="" />
+        </div>
+        <p class="thong-ke__content">Số người thăm thư viện</p>
+      </div>
+      <!-- 2 -->
+      <div class="thong-ke__list bg__section">
+        <div class="thong-ke__soluong">
+          <h2 class="thong-ke__dem">33.17%</h2>
+          <img src="" alt="" />
+        </div>
+        <p class="thong-ke__content">Sách mượn</p>
+      </div>
+      <!-- 3 -->
+      <div class="thong-ke__list bg__section">
+        <div class="thong-ke__soluong">
+          <h2 class="thong-ke__dem">25,119</h2>
+          <img src="" alt="" />
+        </div>
+        <p class="thong-ke__content">Sách quá hạn</p>
+      </div>
+      <!-- phải -->
+    </div>
+  </div>
 </template>
-<script setup lang="ts">
-
-</script>
+<script setup lang="ts"></script>
 
 <style scoped>
+/* thuộc tính chung */
+.bg__section {
+  background-color: #f5f5f5;
+}
+/* -----------------thống kê-------------------- */
+.thongke {
+  width: 20%;
+  display: flex;
+  flex-direction: column;
+  row-gap: 25px;
+}
+.thong-ke__list {
+  padding: 12%;
+  border-radius: 15px;
+}
 </style>
